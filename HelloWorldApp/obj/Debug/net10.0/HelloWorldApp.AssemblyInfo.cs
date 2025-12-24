b5f97149406c93a43d38b86534e9a9e8e98f3a23
kstwkiv <sathwikakurma1@gmail.com>
2025-12-24 11:14:16 +0530

    Sam's Brother Bakery

--- HelloWorldApp/obj/Debug/net10.0/HelloWorldApp.AssemblyInfo.cs
+++ HelloWorldApp/obj/Debug/net10.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c50f83dd005723cd221c8470a653b490f6db924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128dc628b07f91aed6c54a7c524fd21dd59fe9fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Sam Cake/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+public class InvalidFlavourException : Exception
+{
+    public InvalidFlavourException(string message) : base(message)
+    {
+    }
+}
+public class Cake
+{
+    public string Flavour{ get;set; } 
+    public int QuantityInKG{ get;set; } 
+    public double PricePerKG{ get;set; } 
+    public bool CakeOrder()
+    {
+        if (Flavour!="Red Velvet" && Flavour!="Chocolate" && Flavour != "Vanilla")
+        {
+            throw new InvalidFlavourException("Flavour Not Available!");
+        }
+        
+
+        if (QuantityInKG <=0)
+        {
+            throw new Exception("Quantity must be higher!!");
+        }
+
+        return true;
+    }
+    public double PriceCalc()
+    {
+        double disc=0;
+        if (Flavour == "Vanilla")
+        {
+            disc=3;
+        }else if (Flavour == "Chocolate")
+        {
+            disc=7;
+        }else if(Flavour=="Red Velvet")
+        {
+            disc=9;
+        }
+
+        double totalPrice=QuantityInKG*PricePerKG;
+        double discPrice=totalPrice-(totalPrice*disc/100);
+        return discPrice;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Cake cake=new Cake();
+        Console.WriteLine("Enter the Flavour");
+        cake.Flavour=Console.ReadLine();
+        
+        Console.WriteLine("Enter the weight");
+        cake.QuantityInKG=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the price per kg");
+        cake.PricePerKG=double.Parse(Console.ReadLine());
+
+        try
+        {
+            if (cake.CakeOrder())
+            {
+                Console.WriteLine("Cake Ordered Successfully!");
+                double price=cake.PriceCalc();
+                Console.WriteLine("The caker price is "+price);
+            }
+        }
+        catch(InvalidFlavourException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
